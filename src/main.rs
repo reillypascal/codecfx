e1b8a50bbffc0d11da1d8ca999748cdc5ae7da4f
Reilly Spitzfaden <reillypascal@gmail.com>
2025-05-06 23:51:00 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::fs;
+use hound;
+use vox::VoxState;
+
+pub mod vox;
+
+fn main() {
+    let data: Vec<u8> = fs::read("input/libicudata.73.dylib").expect("Error reading file");
+    
+    let mut output: Vec<i16> = Vec::new();
+    let mut vox_state = VoxState::new();
+    
+    for i in 0..data.len() {
+        for nibble in [(data[i] >> 4) & 0xf, data[i] & 0xf].iter() {
+            // vox output is 12-bit, from i16::MIN <-> i16::MAX/2
+            // *don't* shift — changes spectrum, envelope!
+            output.push(vox_state.vox_decode(nibble));
+        }
+    }
+    
+    // write WAV file
+    // spec
+    let spec = hound::WavSpec {
+        channels: 1,
+        sample_rate: 44100,
+        bits_per_sample: 16,
+        sample_format: hound::SampleFormat::Int,
+    };
+    
+    // writer
+    let mut writer = hound::WavWriter::create("output/libicudata.73.wav", spec).expect("Could not create writer");
+    for t in 0..output.len() {
+        writer.write_sample(output[t]).expect("Could not write sample");
+    }
+    writer.finalize().expect("Could not finalize WAV file");
+}
