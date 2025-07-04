4cd0cba5d99d17686f6ff0ca3891ed72f8fd2de1
Reilly Spitzfaden <reillypascal@gmail.com>
2025-07-04 19:24:11 -0400

    iterate through input folder

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,73 @@
 use std::fs;
+use std::path::{self, PathBuf};
+
+use clap::{Parser, ValueEnum};
 use hound;
+use walkdir::WalkDir;
+
 use vox::Vox;
 
 pub mod biquad;
 pub mod vox;
 
 fn main() {
-    let data: Vec<u8> = fs::read("input/beat_1_bip_2_F_mono.wav").expect("Error reading file");
-    
-    let mut encoded: Vec<u8> = Vec::new();
-    let mut output: Vec<i16> = Vec::new();
-
-    let mut filter = biquad::AudioFilter::new();
-    filter.calculate_filter_coeffs();
-
-    let input: Vec<i16> = data.chunks_exact(2)
-        .map(|chunks| {
-            i16::from_le_bytes(chunks.try_into().expect("Could not convert file into 16-bit Vec"))
-        })
-        .collect();
-    
-    // Vox encode/decode
-    let mut vox = Vox::new();
-
-    for sample in input {
-        encoded.push(vox.vox_encode(&sample));
-    }
-
-    for sample in encoded {
-        output.push(vox.vox_decode(&sample));
-    }
-    
-    // filter
-    for i in 0..output.len() {
-        output[i] = filter.process_sample(output[i] as f64) as i16;
-    }
-
-    // write WAV file
-    // spec
-    let spec = hound::WavSpec {
-        channels: 1,
-        sample_rate: 44100,
-        bits_per_sample: 16,
-        sample_format: hound::SampleFormat::Int,
-    };
-    
-    // writer
-    let mut writer = hound::WavWriter::create("output/output.wav", spec).expect("Could not create writer");
-    for t in 0..output.len() {
-        writer.write_sample(output[t]).expect("Could not write sample");
-    }
-    writer.finalize().expect("Could not finalize WAV file");
+    WalkDir::new("input/")
+        .into_iter()
+        .filter_map(|entry| entry.ok())
+        .for_each(|entry| {
+            if let Ok(metadata) = entry.metadata() {
+                if metadata.is_file() {        
+                    let data: Vec<u8> = fs::read(entry.path()).expect("Error reading file");
+                    
+                    let mut encoded: Vec<u8> = Vec::new();
+                    let mut output: Vec<i16> = Vec::new();
+                
+                    let mut filter = biquad::AudioFilter::new();
+                    filter.calculate_filter_coeffs();
+                
+                    let input: Vec<i16> = data.chunks_exact(2)
+                        .map(|chunks| {
+                            i16::from_le_bytes(chunks.try_into().expect("Could not convert file into 16-bit Vec"))
+                        })
+                        .collect();
+                    
+                    // Vox encode/decode
+                    let mut vox = Vox::new();
+                
+                    for sample in input {
+                        encoded.push(vox.vox_encode(&sample));
+                    }
+                
+                    for sample in encoded {
+                        output.push(vox.vox_decode(&sample));
+                    }
+                    
+                    // filter
+                    for i in 0..output.len() {
+                        output[i] = filter.process_sample(output[i] as f64) as i16;
+                    }
+                
+                    let mut write_path = PathBuf::from("output/");
+                    
+                    if let Some(file_name) = entry.path().file_name() {
+                        write_path.push(file_name);
+                        // write WAV file
+                        // spec
+                        let spec = hound::WavSpec {
+                            channels: 1,
+                            sample_rate: 44100,
+                            bits_per_sample: 16,
+                            sample_format: hound::SampleFormat::Int,
+                        };
+                        
+                        // writer
+                        let mut writer = hound::WavWriter::create(write_path, spec).expect("Could not create writer");
+                        for t in 0..output.len() {
+                            writer.write_sample(output[t]).expect("Could not write sample");
+                        }
+                        writer.finalize().expect("Could not finalize WAV file");
+                    }
+                }
+            }
+    });
 }
